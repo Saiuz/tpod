b46ddac394ced27df9127a8005c14a84e2724b1b
junjuew <junjuew@cs.cmu.edu>
2017-06-07 04:19:23 -0400

    add in gunicorn as requirement. update install script to properly hanlde pip

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR
+cd $DIR &&
+source env.sh &&
+. env/bin/activate &&
 ./runcelery.py -A celery_tasks worker --loglevel=info &
 env/bin/gunicorn -b 0.0.0.0:10000 app:app --log-level=debug --timeout=10000 
