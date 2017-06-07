34893c20763e598da1ce129538264a0ff94d6ff9
junjuew <junjuew@cs.cmu.edu>
2017-06-07 04:41:20 -0400

    working run and kill

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR &&
 source env.sh &&
-flask db upgrade &&
 . env/bin/activate &&
+flask db upgrade &&
 ./runcelery.py -A celery_tasks worker --loglevel=info &
 env/bin/gunicorn -b 0.0.0.0:10000 app:app --log-level=debug --timeout=10000 
